e7a95c420d02330f1e1b32699e7371a6b09c443e
Sara Guaiume <randesungasabonete@gmail.com>
2024-08-21 11:00:14 -0300

    projVR

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+ export default function App(){
+  
+  return (
+    <>
+      <h1>Titulo</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis possimus provident delectus, nostrum quis expedita omnis enim facere similique sunt? Accusamus assumenda veniam iste quam similique eos, expedita architecto libero.</p>
+    </>
+    
+  )
+}
+
+// export default App
